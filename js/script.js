129e2bbe4969a9701410189ce24ade8ea1c87218
Ilya-Lonelya <sinner_rrts@mail.ru>
2024-01-26 08:59:38 +0300

    v0.3 fix bird

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ ctx.fillText("Click to Start", 10, 50);
 ctx.fillText("Press any key for fly!", 10, 80);
 
 
-let gap = 130;
+let gap = 130; // The distance between the pipes
 
 document.addEventListener("keydown", moveUp);
 document.addEventListener("click", moveUp);
@@ -51,12 +51,12 @@ pipe[0] = {
     x: cvs.width,
     y: 0
 }
-
 let score = 0;
+let level = 1;
 // Position bird
 let xPos = 10;
 let yPos = 150;
-let grav = 1.2;
+let grav = 1.2; // The speed of gravity
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
@@ -74,30 +74,48 @@ function draw() {
                     pipeUp.height
             });
         }
-        // crash
+        // Checking for pipe or grass collision
         if (xPos + bird.width >= pipe[i].x
             && xPos <= pipe[i].x + pipeUp.width
             && (yPos <= pipe[i].y + pipeUp.height
                 || yPos + bird.height >= pipe[i].y +
                 pipeUp.height + gap)
             || yPos + bird.height >= cvs.height - fg.height) {
-            gameOver.play(); setTimeout(function () {
-                location.reload()
-            }, 150);
+            gameOver.play();
+            return setTimeout(function () {
+                location.reload();
+            }, 160);
         }
         if (pipe[i].x == 10) {
             scoreAudio.play();
             score++;
         }
+        if (score >= 10 && score < 20) {
+            level = 2;
+            gap = 120;
+        }
+        if (score >= 20 && score < 30) {
+            level = 3;
+            gap = 110;
+        }
+        if (score >= 30 && score < 40) {
+            level = 4;
+            gap = 100;
+        }
+        if (score >= 50) {
+            level = "You win!";
+            gap = 90;
+        }
     }
 
     ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, xPos, yPos, 34, 34);
+    ctx.drawImage(bird, xPos, yPos);
 
     yPos += grav;
 
     ctx.fillStyle = "000";
-    ctx.font = "24px Verdana";
-    ctx.fillText("Score: " + score, 10, cvs.height - 8);
+    ctx.font = "22px Verdana";
+    ctx.fillText("Score: " + score, 6, cvs.height - 8);
+    ctx.fillText("Level: " + level, 118, cvs.height - 8);
     requestAnimationFrame(draw);
 }
